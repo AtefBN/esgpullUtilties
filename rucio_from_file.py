de34505eb9c17dcf04c7ac3a52966d488ebbf98c
AtefBN <abennasser@ipsl.fr>
2024-12-10 14:49:11 +0100

    Added file upload prior to attaching dataset to rucio.

--- rucio_from_file.py
+++ rucio_from_file.py
@@ -1,8 +1,11 @@
-from rucio.client import Client
+import rucio.client
+from rucio.client import Client, uploadclient
+from rucio.client.uploadclient import UploadClient
 from rucio.common.exception import DataIdentifierAlreadyExists
-import json
+import json, os
 
 
+import constants
 from utils import parse_file_json, get_files_from_esgpull_query
 from constants import RSE, SCOPE
 
@@ -19,24 +22,50 @@ def get_dataset_list(file_path):
     data = data['49d8b79df01e29fa065ce9d65211d03e98b19750']['files']
     for file in data:
         dataset_id = file['dataset_id']
-        file_id = file['file_id']
+        # file_id = file['file_id']
+        # local_path = file['local_path']
         if dataset_id in dataset_dict.keys():
             files = dataset_dict[dataset_id]
-            files.append(file_id)
+            files.append(file)
         else:
-            files = [file_id]
+            files = [file]
         dataset_dict[dataset_id] = files
     return dataset_dict
 
 
-def attach_datasets_to_rucio(dataset_id, files, rucio_client):
+def attach_datasets_to_rucio(dataset_id, files, rucio_client, upload_client):
+
     try:
         dataset = rucio_client.add_dataset(scope=SCOPE, name=dataset_id, rse=RSE)
     except DataIdentifierAlreadyExists as e:
         print('Dataset already exists, skipping...')
+    # handling file list, uploading then creating dids to attach to dataset
     dids = []
     for file in files:
-        file_dic = {'scope': SCOPE, 'name': file}
+
+        # these lines are commented because they call subprocess instead of rucio python api.
+
+        # try:
+        #     # upload file, like physically upload it
+        #     # rucio upload --rse DESY-DCACHE --scope <YOUR SCOPE> test-file01
+        #     result = subprocess.run(['rucio', 'upload', '--rse', RSE, '--scope', SCOPE, dataset_id])
+        #     if result.returncode != 0:
+        #         print('Error uploading file: {}'.format(result.stderr))
+        # except Exception as e:
+        #     print('Error uploading file: {}'.format(e.message))
+
+        # upload client returns 0 if the upload is successful
+        # raises :
+        # InputValidationError
+        # RSEWriteBlocked
+        # NoFilesUploaded
+        # NotAllFilesUploaded
+        # so catching one of the last two at the very least is necessary before continuing
+
+        # still need the file absolute path before trying to upload.
+        file_path = os.path.join(constants.datapath_prefix, file['local_path'])
+        upload_client(path=file_path, rse=RSE)
+        file_dic = {'scope': SCOPE, 'name': file['file_id']}
         dids.append(file_dic)
 
     attachment = rucio_client.attach_dids(
@@ -56,10 +85,11 @@ def main():
     dataset_dict = get_dataset_list('subset_rucio_cmcc.json')
     print("Init rucio client...")
     rucio_client = Client()
+    upload_client = UploadClient()
     print("Processing dictionary items...")
     for key in dataset_dict.keys():
-        print('now attaching datasets from file {}...'.format(key))
-        attach_datasets_to_rucio(key, dataset_dict[key], rucio_client)
+        print('now attaching files from dataset {}...'.format(key))
+        attach_datasets_to_rucio(key, dataset_dict[key], rucio_client, upload_client)
         print('Dataset has {} files attached.'.format(len(dataset_dict[key])))
         print('----------------------------------------')
 
